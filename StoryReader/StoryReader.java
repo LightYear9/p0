a6b14c5de04d8ec163b46fdd539fd05d16b8bb20
Terry Light <tlight2012@fau.edu>
2021-07-27 00:43:26 -0400

    create class using the file path and delineate words with spaces

--- StoryReader/StoryReader.java
+++ StoryReader/StoryReader.java
@@ -2,19 +2,32 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class ReadFile {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("/users/terrylight/revature/p0/poem.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
+public class StringOccurrence {
+  public static String fileToString(String filePath){
+     Scanner sc = null;
+     String input = null;
+     //adding string buffer to append, concatenate, and manipulate strings
+     StringBuffer sb = null;
+     try {
+        sc = new Scanner(new File(filePath));
+        sb = new StringBuffer();
+        while (sc.hasNextLine()) {
+           input = sc.nextLine();
+           sb.append(" "+input);
+        }
+     }
+     catch(Exception ex) {
+        ex.toString();   
+     }
+     return sb.toString();
+  }
+  public static void main(String args[]) throws FileNotFoundException {
+     String filePath = "/users/terrylight/revature/p0/storyreader/poem.txt";
+     String txt = fileToString(filePath);
+     String arr[] = txt.split(" ");
+     for(String token : arr) {
+        System.out.println(" ");
+        System.out.println(token);
+     }
   }
 }
